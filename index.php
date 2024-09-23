e3c18d6d3270b108d5d9498ec63af106ace8202c
NasiadkaMaciej <maciej.nasiadka@gmail.com>
2024-09-23 20:28:43 +0200

    Huge PHP and SQL safety upgrade

--- index.php
+++ index.php
@@ -8,11 +8,12 @@
 	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
 	<meta name="description" content="Asteroids game clone written in C++ with SFML - Maciej Nasiadka" />
 	<meta property="og:type" content="website" />
-	<meta property="og:title" content="Maciej Nasiadka" />
+	<meta property="og:title" content="Asteroids - Maciej Nasiadka" />
 	<meta property="og:description" content="Asteroids game clone written in C++ with SFML - Maciej Nasiadka" />
+	<meta property="og:image" content="https://nasiadka.pl/asteroids/favicon.png" />
 	<link rel="stylesheet" href="style.css">
 	<link rel="canonical" href="https://nasiadka.pl/asteroids/">
-	<title>Asteroids</title>
+	<title>Asteroids - Maciej Nasiadka</title>
 </head>
 
 <body>
@@ -21,8 +22,8 @@
 		<p id="gameDescription">
 			Destroy as many asteroids as you can with your spaceship's missiles.<br>
 			Be careful! Asteroids aren't the only danger in space. Watch out for evil UFO trying to shoot you down.<br>
-			<a href="AsteroidsWindows.zip">Download game (Windows)</a><br>
-			<a href="AsteroidsLinux.zip">Download game (Linux)</a>
+			<a href="https://github.com/NasiadkaMaciej/Asteroids/releases/download/v1.1.1/AsteroidsWindows.zip">Download game (Windows)</a><br>
+			<a href="https://github.com/NasiadkaMaciej/Asteroids/releases/download/v1.1.1/AsteroidsLinux.zip">Download game (Linux)</a>
 		</p>
 
 		<table id="leaderBoard">
@@ -37,7 +38,7 @@
 			<tbody>
 
 				<?php
-				//Read and show data from server
+				// Read and show data from server
 				include("config.php");
 				$connection = new mysqli($SERVER, $LOGIN, $PASSWORD, $DB);
 				if ($connection->connect_errno) {
@@ -45,53 +46,50 @@
 					exit();
 				}
 
-				if (isset($_GET["pos"]))
-					$pos = $_GET["pos"];
-				else
-					$pos = 0;
+				$pos = isset($_GET["pos"]) ? intval($_GET["pos"]) : 0;
 
-				$query = "SELECT name, points, date FROM leaderboard ORDER BY `leaderboard`.`points` DESC, `leaderboard`.`name` DESC LIMIT " . $pos . ",10";
-				$result = $connection->query($query);
+				$stmt = $connection->prepare("SELECT name, points, date FROM leaderboard ORDER BY points DESC, name DESC LIMIT ?, 10");
+				$stmt->bind_param("i", $pos);
+				$stmt->execute();
+				$result = $stmt->get_result();
+
 				$i = $pos + 1;
 				while ($line = $result->fetch_assoc()) {
-
 					$line["date"] = substr($line["date"], 2);
 
 					echo "<tr>
-					<td class='posTD'>" . $i . "</td>
-					<td class='nameTD'>" . $line["name"] . "</td>
-					<td class='ptsTD'>" . $line["points"] . "</td>
-					<td class='dateTD'>" . $line["date"] . "</td>
+						<td class='posTD'>" . htmlspecialchars($i) . "</td>
+						<td class='nameTD'>" . htmlspecialchars($line["name"]) . "</td>
+						<td class='ptsTD'>" . htmlspecialchars($line["points"]) . "</td>
+						<td class='dateTD'>" . htmlspecialchars($line["date"]) . "</td>
 					</tr>";
-					$i = $i + 1;
+					$i++;
 				}
 				?>
+
 				<tr>
 					<td>
 						<?php
-						//Handle going back in leaderboard
+						// Handle going back in leaderboard
 						$prevpos = $pos - 10;
 						if ($prevpos > 0)
 							echo '<a href="/asteroids/?pos=' . $prevpos . '">Back</a>';
-						elseif ($prevpos == 0)
+						else if($prevpos == 0)
 							echo '<a href="/asteroids/">Back</a>';
-
 						?>
 					</td>
 					<td></td>
 					<td>
 						<?php
-						//Handle going to next page in leaderboard
-						$query = "SELECT * FROM leaderboard";
+						// Handle going to the next page in leaderboard
+						$query = "SELECT COUNT(*) FROM leaderboard";
 						$result = $connection->query($query);
-						$rowcount = mysqli_num_rows($result);
-						//$maxValue =  round($rowcount, -1, PHP_ROUND_HALF_UP);
+						$rowcount = $result->fetch_row()[0];
 						$maxValue = ceil($rowcount / 10) * 10;
-						//$maxValue =  ceil($rowcount)
 						$nextpos = $pos + 10;
+
 						if ($nextpos < $maxValue)
 							echo '<a href="/asteroids/?pos=' . $nextpos . '">Next</a>';
-
 						$connection->close();
 						?>
 					</td>
@@ -123,17 +121,19 @@
 			X - Shoot<br>
 			Triangle - New game<br>
 			START - Entering menu<br>
-			Arrows - Navigate in menu O - Exiting menu<br>
+			Arrows - Navigate in menu<br>
+			O - Exiting menu<br>
 			X - Choosing menu entry<br>
 			L1 - Toggling fullscreen<br>
-			R1 - Toggle music at any time</p>
+			R1 - Toggle music at any time
+		</p>
 	</div>
 	<p style="text-align: center;">
-		<iframe style="width: 63vw; height: 27vw" src="https://www.youtube.com/embed/zexjrqSEklM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		<iframe style="width: 84vw; height: 36vw" src="https://www.youtube.com/embed/zexjrqSEklM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 	</p>
 	<p id="completeGameDescription">
 		Asteroids game clone. Written in C++ with SFML.<br>
-		Destroy as many asteroids as you can with your spaceship's missiles, but be careful, they come from all sides, at different speeds and, when destroyed, they break into more, even faster and more dangerous fragments. Be cautious! Once in a while, a mysterious evil UFO shows up and starts shooting to you. Each level ends after destroying all visible asteroids. After that, you get into the next level, which has 2 more asteroids than the previous one. You start the game with 3 lives, after you lose one, you enter idle state, in which asteroids cannot hurt you, but you can't shoot them. To continue the game, simply move your ship. When you lose all your lives you can save your score and send it to Asteroids online leaderboard
+		Destroy as many asteroids as you can with your spaceship's missiles, but be careful, they come from all sides, at different speeds and, when destroyed, they break into more, even faster and more dangerous fragments. Be cautious! Once in a while, a mysterious evil UFO shows up and starts shooting at you. Each level ends after destroying all visible asteroids. After that, you get into the next level, which has 2 more asteroids than the previous one. You start the game with 3 lives; after you lose one, you enter idle state, in which asteroids cannot hurt you, but you can't shoot them. To continue the game, simply move your ship. When you lose all your lives, you can save your score and send it to the Asteroids online leaderboard.
 	</p>
 	<footer>
 		<p><a href="https://nasiadka.pl/">Maciej Nasiadka © 2023</a></p>
